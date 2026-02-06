f738ebf9bbcbe07d6991e31c9ad904b9731f1aa5
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-06 22:45:50 +0600

    QuerySellector code added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Headding Tag code now;
 let heading = document.getElementsByTagName("h2");
-for(let i = 0; i <= heading.length;i++){
+for (let i = 0; i <= heading.length; i++) {
     // console.log(heading[i]);
 }
 // box code here now:
@@ -15,7 +15,7 @@ let all = document.getElementsByTagName("*");
 // li code now:
 let list = document.getElementsByTagName("li");
 // console.log(list);
-for(let i = 0; i  < list.length; i++){
+for (let i = 0; i < list.length; i++) {
     // console.log(list[i].style.color='red');
 }
 // console.log(list[4].innerHTML);
@@ -24,10 +24,10 @@ for(let i = 0; i  < list.length; i++){
 // Task code here:
 let items = document.getElementsByTagName("li");
 console.log(items);
-for(let l = 0; l < items.length; l++){
+for (let l = 0; l < items.length; l++) {
     console.log(items[l].innerText);
-    if(items[l].innerText === "JavaScript"){
-        items[l].style.color='green'
+    if (items[l].innerText === "JavaScript") {
+        items[l].style.color = 'green'
     }
 }
 // let li = ul.getElementsByTagName("li");
@@ -38,9 +38,29 @@ let boxing = document.getElementById('boxing');
 // console.log(boxing);
 let button = boxing.getElementsByTagName("button");
 // console.log(button);
-for(let b = 0; b < button.length; b++){
-    console.log(button[b].style.background='black');
-    button[b].style.color='white'
-    button[b].style.padding='10px'
+for (let b = 0; b < button.length; b++) {
+    console.log(button[b].style.background = 'black');
+    button[b].style.color = 'white'
+    button[b].style.padding = '10px'
     // button[b.style.color='yellow']
+}
+// Class Sellector:
+let foods = document.getElementsByClassName("food");
+console.log(foods);
+for (let food of foods) {
+    console.log(food.innerText);
+}
+// let ul = foods.getElementsByTagName("ul");
+// console.log(ul);
+let foods2 = document.querySelector(".food");
+console.log(foods2);
+
+// QuerySellector and QuerSellectorAll Example Code here now:
+const change = () => {
+    let title = document.querySelector(".title");
+    if(title.style.color==='green'){
+        title.style.color='red'
+    }else{
+        title.style.color='green'
+    }
 }
